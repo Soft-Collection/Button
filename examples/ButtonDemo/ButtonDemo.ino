786be22f15aaed7a38ae8f61bca5d06d9ad4c676
Soft-Collection <121801033+Soft-Collection@users.noreply.github.com>
2025-01-20 22:14:43 +0200

    Update ButtonDemo.ino

--- examples/ButtonDemo/ButtonDemo.ino
+++ examples/ButtonDemo/ButtonDemo.ino
@@ -4,6 +4,14 @@
 static void on_button(void* instance, String name, bool pressed, bool firstTime);
 static bool on_get_button_state(String name);
 
+//Parameters are:
+//1. Calling instance
+//2. Button name
+//3. If is inverted pin, then true else false
+//4. Delay until first reaction in 10 milliseconds
+//5. Delay after first reaction in 10 milliseconds
+//6. Callback function
+//7. Function that decides whether the button is pressed
 Button button1(NULL, "Button1", false, 70, 10, on_button, on_get_button_state);
 
 void setup() {
